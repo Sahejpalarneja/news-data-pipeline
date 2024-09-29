726e354d3396a9238a42618b53e24e2883462f4e
Sahejpal Arneja <sahejpalarneja@gmail.com>
2024-09-29 16:49:24 +0200

    Normalized text and improved logging

--- cleaner/cleaners.py
+++ cleaner/cleaners.py
@@ -1,8 +1,17 @@
 import logging
 import re
+import unicodedata
+import unidecode
 
 logging.basicConfig(filename="cleaner/results/BaseCleaner.log", level=logging.INFO, filemode = "w+")
 
+
+def normalize_text(text: str) -> str:
+    text = unicodedata.normalize("NFKD", text)
+    text = unidecode.unidecode(text)
+    text = re.sub('"', ' ', text)
+    text = re.sub("'", ' ', text)
+    return text
 
 class NBCCleaner:
     def __init__(self, resultConfig, url) -> None:
@@ -14,33 +23,28 @@ class NBCCleaner:
             article['title'] = self._cleanTitle(article['title'])
             article['text'] = self._cleanText(article['text'])
             article['date'] = self._cleanDate(article['date'])
+            logging.info(f"Cleaned Article. {article['URL']}")
         return self.articles
-
 
     def _cleanTitle(self, title: str) -> str:
         try:
-            title = re.sub('"', ' ', title)
-            title = re.sub("'", ' ', title)
-            title = re.sub("”", ' ', title)
+            title = normalize_text(title)
         except Exception:
-            logging.warn(f"Could not clean title for {self.baseURL}")
+            logging.warning(f"Could not clean title for {self.baseURL}")
         return title
-
 
     def _cleanText(self, text: str) -> str:
         try:
-            text = re.sub('"', ' ', text)
-            text = re.sub("'", ' ', text)
-            text = re.sub("”", ' ', text)
+            text = normalize_text(text)
         except Exception:
-            logging.warn(f"Could not clean text for {self.baseURL}")
+            logging.warning(f"Could not clean text for {self.baseURL}")
         return text
     
     def _cleanDate(self, date: str) -> str:
         try:
             date = date.split('T')[0]
         except:
-            logging.warn(f"Could not clean date for {self.baseURL}")
+            logging.warning(f"Could not clean date for {self.baseURL}")
         return date
 
     
@@ -56,32 +60,34 @@ class NewsWeekCleaner:
             article['title'] = self._cleanTitle(article['title'])
             article['text'] = self._cleanText(article['text'])
             article['date'] = self._cleanDate(article['date'])
+            article['author'] = self._cleanAuthor(article['author'])
+            logging.info(f"Cleaned Article. {article['URL']}")
         return self.articles
-
 
     def _cleanTitle(self, title: str) -> str:
         try:
-            title = re.sub('"', ' ', title)
-            title = re.sub("'", ' ', title)
-            title = re.sub("”", ' ', title)
-            
+            title = normalize_text(title)
         except Exception:
-            logging.warn(f"Could not clean title for {self.baseURL}")
+            logging.warning(f"Could not clean title for {self.baseURL}")
         return title
-
 
     def _cleanText(self, text: str) -> str:
         try:
-            text = re.sub('"', ' ', text)
-            text = re.sub("'", ' ', text)
-            text = re.sub("”", ' ', text)
+            text = normalize_text(text)
         except Exception:
-            logging.warn(f"Could not clean text for {self.baseURL}")
+            logging.warning(f"Could not clean text for {self.baseURL}")
         return text
 
     def _cleanDate(self, date: str) -> str:
         try:
             date = date.split('T')[0]
         except:
-            logging.warn(f"Could not clean date for {self.baseURL}")
+            logging.warning(f"Could not clean date for {self.baseURL}")
         return date
+    
+    def _cleanAuthor(self, author: str) -> str:
+        try:
+            author = normalize_text(author)
+        except:
+            logging.warning(f"Could not clean author for {self.baseURL}")
+        return author
